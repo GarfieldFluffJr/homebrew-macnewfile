f9b7f0b32d69121fbbda588bd3583d7ba9882492
Louie <louieyin6@gmail.com>
2026-01-30 21:17:34 -0500

    fix sha256 spacing

--- Casks/macnewfile.rb
+++ Casks/macnewfile.rb
@@ -1,6 +1,6 @@
 cask "macnewfile" do                                                                                                          
     version "2.1.0"                                                                                                             
-    sha256 "8be7b5ad741389a370b0d0efa1e79ad8cbfc29fc7b68ab46d8c600eb7f5ca419       "                                                   
+    sha256 "8be7b5ad741389a370b0d0efa1e79ad8cbfc29fc7b68ab46d8c600eb7f5ca419"                                                   
                                                                                                                                 
     url "https://github.com/GarfieldFluffJr/MacNewFile/releases/download/v#{version}/MacNewFile.zip"                            
     name "MacNewFile"                                                                                                           
